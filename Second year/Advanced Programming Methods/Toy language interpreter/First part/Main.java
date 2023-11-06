7e1e02e8c6f7b6f106e0e5c31f825bf2b02e1e1c
Teodora Vlad <115424811+TeodoraVlad12@users.noreply.github.com>
2023-11-06 15:40:09 +0200

    Create Main.java

---
+++ Second year/Advanced Programming Methods/Toy language interpreter/First part/Main.java
@@ -0,0 +1,10 @@
+import view.View;
+
+// Press Shift twice to open the Search Everywhere dialog and model.type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        View view = new View();
+        view.start();
+    }
+}
