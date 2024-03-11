d6d8393d53f057a04d92c34d193d1d5087aac6c6
Teodora Vlad <115424811+TeodoraVlad12@users.noreply.github.com>
2024-03-12 00:42:17 +0200

    Create App.tsx

---
+++ Second year/Systems for Design and Implementation/FirstAssignment- CRUD/App.tsx
@@ -0,0 +1,12 @@
+import Message from './components/Message.tsx'
+import CRUD from "./components/CRUD.tsx";
+//import List from "./components/List.tsx";
+//import Add from "./components/List.tsx";
+function App(){
+    return <div>  <Message />   <CRUD/></div>
+}
+
+export default App;
+
+
+//tb teste unittesting
