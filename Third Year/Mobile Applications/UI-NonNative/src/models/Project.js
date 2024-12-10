3f6f26f63883787f91b93999c5672a8427c9c02b
Teodora Vlad <115424811+TeodoraVlad12@users.noreply.github.com>
2024-12-10 09:45:34 +0200

    Create Project.js

---
+++ Third Year/Mobile Applications/UI-NonNative/src/models/Project.js
@@ -0,0 +1,10 @@
+export class Project {
+  constructor(id, name, description, startDate, endDate, urgency) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.urgency = urgency;
+  }
+}
